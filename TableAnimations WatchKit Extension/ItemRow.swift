6537e4c3729273f7f8ba7bf34c5e99f12967ca46
Sowaka <sowaka@Takeshi-Oogami-no-MacBook-Pro.local>
2015-07-22 18:52:47 +0900

    Added table animation

---
+++ TableAnimations WatchKit Extension/ItemRow.swift
@@ -0,0 +1,15 @@
+//
+//  ItemRow.swift
+//  TableAnimations
+//
+//  Created by Sowaka on 2015/07/22.
+//  Copyright (c) 2015年 com.myCompany. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class ItemRow: NSObject {
+    @IBOutlet weak var name: WKInterfaceLabel!
+    @IBOutlet weak var rank: WKInterfaceLabel!
+}
---
+++ TableAnimations WatchKit Extension/MessageRow.swift
@@ -0,0 +1,15 @@
+//
+//  MessageRow.swift
+//  TableAnimations
+//
+//  Created by Sowaka on 2015/07/22.
+//  Copyright (c) 2015年 com.myCompany. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class MessageRow: NSObject {
+    @IBOutlet weak var image: WKInterfaceImage!
+    @IBOutlet weak var label: WKInterfaceLabel!
+}
