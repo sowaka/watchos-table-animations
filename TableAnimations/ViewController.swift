6080358715816f5fd91021ab93522210d534324d
Sowaka <sowaka@Takeshi-Oogami-no-MacBook-Pro.local>
2015-07-22 18:08:20 +0900

    Initial Commit

---
+++ TableAnimations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableAnimations
+//
+//  Created by Sowaka on 2015/07/22.
+//  Copyright (c) 2015年 com.myCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
